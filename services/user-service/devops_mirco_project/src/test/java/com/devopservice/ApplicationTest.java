605f102eb1d7f2fbb49dd0b7e249e674cda68bc0
Simon <64027803+simonsimme@users.noreply.github.com>
2025-08-23 21:45:05 +0200

    Ci update

--- services/user-service/devops_mirco_project/src/test/java/com/devopservice/ApplicationTest.java
+++ services/user-service/devops_mirco_project/src/test/java/com/devopservice/ApplicationTest.java
@@ -2,10 +2,20 @@ package com.devopservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@ActiveProfiles("test")
+@TestPropertySource(properties = {
+    "spring.datasource.url=jdbc:h2:mem:testdb",
+    "spring.datasource.username=sa",
+    "spring.datasource.password=password",
+    "spring.datasource.driver-class-name=org.h2.Driver",
+    "spring.jpa.hibernate.ddl-auto=create-drop",
+    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+    "spring.flyway.enabled=false",
+    "app.jwt.secret=test-secret",
+    "app.jwt.expiryMinutes=30"
+})
 class ApplicationTest {
 
     @Test
